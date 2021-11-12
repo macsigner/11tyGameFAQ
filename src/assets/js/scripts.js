85600b7d03af56d26f1cc4be3d77f185f32d28a8
Mac Signer <macsigner@gmx.ch>
2021-11-12 20:53:30 +0100

    Add underline vars

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -2,3 +2,26 @@
 
 document.documentElement.classList.remove('no-js');
 document.documentElement.classList.add('js');
+
+class UnderlineVars {
+    constructor(el) {
+        this.el = el;
+        this.el.style.setProperty('--underlines', this.getLengthAsSymbol())
+        this.el.style.setProperty('--lines', this.getLengthAsSymbol())
+    }
+
+    getLengthAsSymbol(strSymbol = '_') {
+        let str = '';
+        let length = this.el.innerHTML.length;
+
+        for (let i = 0; i < length; i++) {
+            str += strSymbol;
+        }
+
+        return str;
+    }
+}
+
+const nlUnderlines = document.querySelectorAll('h1,h2,h3,h4,h5,h6');
+
+nlUnderlines.forEach((el) => new UnderlineVars(el));
