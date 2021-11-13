203c020267a4a1ce9f21f413c1db847d2f78cb9e
Mac Signer <macsigner@gmx.ch>
2021-11-13 12:01:38 +0100

    Add console log funtionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
-const markdownIt = require('markdown-it')
-const markdownItAnchor = require('markdown-it-anchor')
-const pluginTOC = require('eleventy-plugin-toc')
-
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const pluginTOC = require('eleventy-plugin-toc');
+const util = require('util');
 
 module.exports = function(eleventyConfig) {
+    // Add console
+    eleventyConfig.addFilter('console', function(value) {
+        const str = util.inspect(value);
+        console.log(str);
+    });
     eleventyConfig.addPlugin(pluginTOC);
     // Disable ignoring files to keep walkthrough content seperate
     eleventyConfig.setUseGitIgnore(false);
