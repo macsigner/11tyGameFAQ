096a4c381962790242fb5e553220959894eb43f0
Mac Signer <macsigner@gmx.ch>
2021-11-13 21:38:23 +0100

    Add scroll anchors

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -26,3 +26,60 @@ class UnderlineVars {
 const nlUnderlines = document.querySelectorAll('h1,h2,h3,h4,h5,h6');
 
 nlUnderlines.forEach((el) => new UnderlineVars(el));
+
+const DelayedScroll = function(options = {}) {
+    const _ = this;
+    /**
+     * Init.
+     */
+    _.init = function() {
+        _._defaultSettings = {
+            delay: 100,
+            scrollElement: window,
+        };
+        _.settings = _._defaultSettings;
+
+        if (typeof (Event) === 'function') {
+            _.delayedScrollEvent = new Event('delayedScroll');
+        } else {
+            _.delayedScrollEvent = document.createEvent('Event');
+            _.delayedScrollEvent.initEvent('delayedScroll', true, true);
+        }
+
+        let timeout;
+
+        window.addEventListener('scroll', function() {
+            clearTimeout(timeout);
+
+            timeout = setTimeout(function() {
+                window.dispatchEvent(_.delayedScrollEvent);
+            }, _.settings.delay);
+        });
+    };
+
+    _.init();
+};
+
+new DelayedScroll();
+
+class UpdateAnchor {
+    constructor(el) {
+        this.el = el;
+        this.anchors = this.el.querySelectorAll('h1[id],h2[id],h3[id]');
+
+        window.addEventListener('delayedScroll', () => {
+            let offset = document.documentElement.scrollTop + 16;
+
+            this.anchors.forEach((el, i) => {
+                let next = this.anchors[i + 1];
+                if (el.offsetTop < offset && next && next.offsetTop > offset) {
+                    history.replaceState(null, el.innerHTML, '#' + el.id);
+                }
+            });
+        })
+    }
+}
+
+const nlUpdateAnchors = document.querySelectorAll('[data-update-anchor]');
+
+nlUpdateAnchors.forEach(el => new UpdateAnchor(el));
